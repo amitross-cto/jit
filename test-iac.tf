e5df34972b43eb45bafc6d380f76a4179fe030b3
Amit Ross <amitr@jit.io>
2025-06-16 09:59:55 +0300

    Delete test-iac.tf

--- test-iac.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "insecure_bucket" {
-  bucket = "my-insecure-bucket"
-  acl    = "public-read"   # <-- Vulnerability: Publicly accessible bucket
-
-  tags = {
-    Name        = "InsecureBucket"
-    Environment = "Dev"
-  }
-}
